84a22618d748984fdd2244e938da2135ab80bc24
YaninaMatsiash <yanina.matyash@gmail.com>
2022-11-30 21:29:38 +0100

    add js slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-<script>document.getElementById('davaToday').valueAsDate = new Date();</script>
+document.getElementById('davaToday').valueAsDate = new Date();
+
+
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide');
+
+    slides[activeSlide].classList.add('active');
+
+    for (const slide of slides) {
+        if (slide.classList.contains('active')) {
+            document.body.style.background = "#F2F2F2";
+        }
+        slide.addEventListener('click', () => {
+            clearActiveClasses();    
+            slide.classList.add('active');
+            document.body.style.background = "#F2F2F2";
+        });
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+    }
+}
+
+slidesPlugin(4);
